d1b73975ad3897d2c52e95ca093dc51ada604e3d
andrew <a.myrold@att.net>
2024-08-01 15:54:08 -0400

    Add User input validation on user login backend endpoint - fixed session routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,17 +2,15 @@
 const express = require("express");
 const router = express.Router();
 
-const apiRouter = require('./api');
-router.use('/api', apiRouter);
-// ...
-
+const apiRouter = require("./api");
+router.use("/api", apiRouter);
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
   res.cookie("XSRF-TOKEN", csrfToken);
   res.status(200).json({
-    'XSRF-Token': csrfToken
+    "XSRF-Token": csrfToken,
   });
 });
 
