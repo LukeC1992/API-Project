4c6b46001a8f5f08a1d203a16a1f7dcd6458a098
andrew <a.myrold@att.net>
2024-08-08 11:30:50 -0400

    why don't you work

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -63,7 +63,10 @@ const restoreUser = (req, res, next) => {
 
 // If there is no current user, return an error
 const requireAuth = function (req, _res, next) {
-  if (req.user) return next();
+  if (req.user) {
+    console.log(req.user);
+    return next();
+  }
 
   const err = new Error("Authentication required");
   err.status = 401;
